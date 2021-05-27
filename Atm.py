adcf4557af150e2147d11f38e7201977480d5a9f
anvita-v <69374399+anvita-v@users.noreply.github.com>
2021-05-27 16:31:37 -0700

    Add files via upload

---
+++ Atm.py
@@ -0,0 +1,10 @@
+class atm(object):
+    def __init__(self, card_num, pin_num):
+        self.card_num = card_num
+        self.pin_num = pin_num
+
+    def CashWithdrawl(self):
+        print("withdrawing")
+
+    def BalanceEnquiry(self):
+        print("wnquiry")
